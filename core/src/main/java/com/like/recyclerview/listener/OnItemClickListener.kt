8f2a4ed36bb3f612da821ad57759860dacdd5f0b
like <like@adtech.com.cn>
2020-09-10 10:22:06 +0800

    升级 kotlin 1.4.0

--- core/src/main/java/com/like/recyclerview/listener/OnItemClickListener.kt
+++ core/src/main/java/com/like/recyclerview/listener/OnItemClickListener.kt
@@ -3,6 +3,6 @@ package com.like.recyclerview.listener
 import com.like.recyclerview.model.IRecyclerViewItem
 import com.like.recyclerview.viewholder.CommonViewHolder
 
-interface OnItemClickListener {
+fun interface OnItemClickListener {
     fun onItemClick(holder: CommonViewHolder, position: Int, data: IRecyclerViewItem?)
 }
--- core/src/main/java/com/like/recyclerview/listener/OnItemLongClickListener.kt
+++ core/src/main/java/com/like/recyclerview/listener/OnItemLongClickListener.kt
@@ -3,6 +3,6 @@ package com.like.recyclerview.listener
 import com.like.recyclerview.model.IRecyclerViewItem
 import com.like.recyclerview.viewholder.CommonViewHolder
 
-interface OnItemLongClickListener {
+fun interface OnItemLongClickListener {
     fun onItemLongClick(holder: CommonViewHolder, position: Int, data: IRecyclerViewItem?)
 }
