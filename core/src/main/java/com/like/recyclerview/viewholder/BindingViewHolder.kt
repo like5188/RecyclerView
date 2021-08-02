7b908f82ff2221c85c9485c71d9745ab7a99275a
like5188 <260864866@qq.com>
2021-08-02 16:23:42 +0800

    测试ConcatAdapter

--- core/src/main/java/com/like/recyclerview/viewholder/BindingViewHolder.kt
+++ core/src/main/java/com/like/recyclerview/viewholder/BindingViewHolder.kt
@@ -6,4 +6,4 @@ import androidx.recyclerview.widget.RecyclerView
 /**
  * 通用的结合dataBinding的RecyclerView的ViewHolder。实际上只是持有ViewDataBinding的引用
  */
-open class BindingViewHolder<T : ViewDataBinding>(val binding: T) : RecyclerView.ViewHolder(binding.root)
+open class BindingViewHolder<VB : ViewDataBinding>(val binding: VB) : RecyclerView.ViewHolder(binding.root)
