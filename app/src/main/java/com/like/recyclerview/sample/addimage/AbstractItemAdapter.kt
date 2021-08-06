bba3e9bd66d2068cdbf72ce5653a1562acc9810b
like5188 <260864866@qq.com>
2021-08-06 12:35:09 +0800

    测试ConcatAdapter

---
+++ app/src/main/java/com/like/recyclerview/sample/addimage/AbstractItemAdapter.kt
@@ -0,0 +1,91 @@
+package com.like.recyclerview.sample.addimage
+
+import android.widget.Toast
+import androidx.databinding.ObservableBoolean
+import coil.load
+import com.like.common.util.CoilEngine
+import com.like.recyclerview.ext.adapter.DragAdapter
+import com.like.recyclerview.sample.R
+import com.like.recyclerview.sample.databinding.ViewImageBinding
+import com.like.recyclerview.viewholder.BindingViewHolder
+import com.luck.picture.lib.PictureSelector
+import com.luck.picture.lib.entity.LocalMedia
+import java.io.File
+
+abstract class AbstractItemAdapter(
+    private val activity: PictureSelectorActivity,
+) : DragAdapter<ViewImageBinding, AddImageViewInfo>() {
+    private val maxImageCount = 9
+    val showDeleteButton: ObservableBoolean = ObservableBoolean()
+
+    fun getLocalMedias() = mList.map {
+        it.localMedia
+    }
+
+    override fun onBindViewHolder(holder: BindingViewHolder<ViewImageBinding>, position: Int) {
+        super.onBindViewHolder(holder, position)
+        val item = get(position) ?: return
+        val binding = holder.binding
+        binding.iv.load(File(item.compressImagePath))
+        binding.tv.text = item.des
+        binding.root.setOnLongClickListener {
+            // 显示删除按钮
+            if (!showDeleteButton.get()) {
+                showDeleteButton.set(true)
+            }
+            true
+        }
+        binding.root.setOnClickListener {
+            val localMedias = getLocalMedias()
+            // 预览图片 可自定长按保存路径
+            // 注意 .themeStyle(R.style.theme)；里面的参数不可删，否则闪退...
+            PictureSelector.create(activity)
+                .themeStyle(R.style.picture_default_style)
+                .isNotPreviewDownload(true)
+                .imageEngine(CoilEngine.create()) // 请参考Demo GlideEngine.java
+                .openExternalPreview(position, localMedias)
+        }
+        binding.ivDelete.setOnClickListener {
+            removeItem(item)
+        }
+        binding.deleteButtonShown = showDeleteButton
+    }
+
+    fun addLocalMedias(list: List<LocalMedia>) {
+        val localMedias = getLocalMedias()
+        // 去掉已经添加过的
+        val items = list.filter { !localMedias.contains(it) }.map {
+            AddImageViewInfo(it, "des")
+        }
+        val curCount = mList.size
+        when {
+            curCount + items.size < maxImageCount -> {
+                addAllToEnd(items)
+            }
+            curCount + items.size == maxImageCount -> {
+                notifyRemovePlus()
+                addAllToEnd(items)
+            }
+            else -> {// 不能添加
+                Toast.makeText(activity, "只能添加 $maxImageCount 张图片", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun removeItem(item: AddImageViewInfo) {
+        val curCount = mList.size
+        when {
+            curCount < maxImageCount -> {// 不添加+号图片
+                remove(item)
+            }
+            curCount == maxImageCount -> {// 添加+号图片
+                remove(item)
+                notifyAddPlus()
+            }
+        }
+    }
+
+    abstract fun notifyRemovePlus()
+    abstract fun notifyAddPlus()
+
+}
---
+++ app/src/main/java/com/like/recyclerview/sample/addimage/AbstractPlusAdapter.kt
@@ -0,0 +1,77 @@
+package com.like.recyclerview.sample.addimage
+
+import android.util.Log
+import androidx.annotation.DrawableRes
+import com.like.common.util.CoilEngine
+import com.like.recyclerview.adapter.AbstractAdapter
+import com.like.recyclerview.sample.databinding.ViewAddImageBinding
+import com.like.recyclerview.viewholder.BindingViewHolder
+import com.luck.picture.lib.PictureSelector
+import com.luck.picture.lib.config.PictureConfig
+import com.luck.picture.lib.config.PictureMimeType
+import com.luck.picture.lib.entity.LocalMedia
+import com.luck.picture.lib.listener.OnResultCallbackListener
+
+abstract class AbstractPlusAdapter(
+    private val activity: PictureSelectorActivity,
+    @DrawableRes addImageResId: Int
+) : AbstractAdapter<ViewAddImageBinding, AddInfo>() {
+
+    init {
+        addToEnd(AddInfo(addImageResId))
+    }
+
+    override fun onBindViewHolder(holder: BindingViewHolder<ViewAddImageBinding>, position: Int) {
+        super.onBindViewHolder(holder, position)
+        val item = get(position) ?: return
+        val binding = holder.binding
+        binding.iv.setImageResource(item.addImageResId)
+        binding.iv.setOnClickListener {
+            val localMedias = getLocalMedias()
+            PictureSelector.create(activity)
+                .openGallery(PictureMimeType.ofImage())
+                .maxSelectNum(9)
+                .imageEngine(CoilEngine.create())
+                .selectionData(localMedias)
+                .imageSpanCount(3)// 每行显示个数 int
+                .selectionMode(PictureConfig.MULTIPLE)// 多选 or 单选 PictureConfig.MULTIPLE or PictureConfig.SINGLE
+                .isPreviewImage(true)// 是否可预览图片 true or false
+                .isCamera(true)// 是否显示拍照按钮 true or false
+                .isZoomAnim(true)// 图片列表点击 缩放效果 默认true
+                .isCompress(true)// 是否压缩 true or false
+                .isPreviewEggs(true)// 预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中) true or false
+                .minimumCompressSize(100)// 小于100kb的图片不压缩
+                .forResult(object : OnResultCallbackListener<LocalMedia> {
+                    override fun onResult(result: MutableList<LocalMedia>?) {
+                        if (result.isNullOrEmpty()) return
+                        // 例如 LocalMedia 里面返回五种path
+                        // 1.media.getPath(); 为原图path
+                        // 2.media.getCutPath();为裁剪后path，需判断media.isCut();是否为true
+                        // 3.media.getCompressPath();为压缩后path，需判断media.isCompressed();是否为true
+                        // 4.media.getOriginalPath()); media.isOriginal());为true时此字段才有值
+                        // 5.media.getAndroidQToPath();为Android Q版本特有返回的字段，此字段有值就用来做上传使用
+                        // 如果同时开启裁剪和压缩，则取压缩路径为准因为是先裁剪后压缩
+                        for (media in result) {
+                            Log.i(PictureSelectorActivity.TAG, "是否压缩:" + media.isCompressed)
+                            Log.i(PictureSelectorActivity.TAG, "压缩:" + media.compressPath)
+                            Log.i(PictureSelectorActivity.TAG, "原图:" + media.path)
+                            Log.i(PictureSelectorActivity.TAG, "是否裁剪:" + media.isCut)
+                            Log.i(PictureSelectorActivity.TAG, "裁剪:" + media.cutPath)
+                            Log.i(PictureSelectorActivity.TAG, "是否开启原图:" + media.isOriginal)
+                            Log.i(PictureSelectorActivity.TAG, "原图路径:" + media.originalPath)
+                            Log.i(PictureSelectorActivity.TAG, "Android Q 特有Path:" + media.androidQToPath)
+                        }
+                        addItems(result)
+                    }
+
+                    override fun onCancel() {
+                    }
+                })
+            onAddClicked()
+        }
+    }
+
+    abstract fun getLocalMedias(): List<LocalMedia>
+    abstract fun addItems(list: List<LocalMedia>)
+    abstract fun onAddClicked()
+}
