f84f5cf7ca5c77c94281ce70b67f454062c95287
like5188 <260864866@qq.com>
2022-06-13 16:55:13 +0800

    paging3支持

--- core/src/main/java/com/like/recyclerview/adapter/BaseLoadStateAdapter.kt
+++ core/src/main/java/com/like/recyclerview/adapter/BaseLoadStateAdapter.kt
@@ -14,20 +14,15 @@ import com.like.recyclerview.viewholder.BindingViewHolder
 
 abstract class BaseLoadStateAdapter<VB : ViewDataBinding>(@LayoutRes private val layoutId: Int) :
     LoadStateAdapter<BindingViewHolder<VB>>() {
-    //记录列表adapter的loadState
-    private var outLoadStates: CombinedLoadStates? = null
-
-    //记录自身是否被添加进RecycleView
+    private var refreshLoadState: LoadState? = null
     private var hasInserted = false
 
     init {
-        //注册监听，记录是否被添加
         this.registerAdapterDataObserver(
             object : RecyclerView.AdapterDataObserver() {
 
                 override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
                     super.onItemRangeInserted(positionStart, itemCount)
-                    Logger.e("onItemRangeInserted")
                     hasInserted = true
                 }
 
@@ -65,16 +60,12 @@ abstract class BaseLoadStateAdapter<VB : ViewDataBinding>(@LayoutRes private val
         }
     }
 
-    //更新外部LoadState
-    fun updateLoadState(loadState: CombinedLoadStates) {
-        outLoadStates = loadState
+    fun updateLoadState(states: CombinedLoadStates) {
+        this.refreshLoadState = states.refresh
     }
 
-    //重写，增加判断逻辑
     override fun displayLoadStateAsItem(loadState: LoadState): Boolean {
-        //新增逻辑，refresh状态为NotLoading之后，NotLoading再显示footer
-        val resultB = (loadState is LoadState.NotLoading && outLoadStates?.refresh is LoadState.NotLoading)
-        val result = super.displayLoadStateAsItem(loadState) || resultB
+        val result = super.displayLoadStateAsItem(loadState) || refreshLoadState is LoadState.NotLoading
         if (result && !hasInserted) {
             notifyItemInserted(0)
         }
