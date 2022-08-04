1ffe8a2c2173648070d46ec8c4d253017c36665e
like5188 <260864866@qq.com>
2022-08-04 21:59:02 +0800

    完善

--- core/src/main/java/com/like/recyclerview/utils/ConcatAdapter.kt
+++ core/src/main/java/com/like/recyclerview/utils/ConcatAdapter.kt
@@ -9,12 +9,6 @@ fun ConcatAdapter.clear() {
     }
 }
 
-fun ConcatAdapter.addIfAbsent(adapter: RecyclerView.Adapter<out RecyclerView.ViewHolder>?) {
-    if (!contains(adapter)) {
-        add(adapter)
-    }
-}
-
 fun ConcatAdapter.contains(adapter: RecyclerView.Adapter<out RecyclerView.ViewHolder>?): Boolean {
     adapter ?: return false
     adapters.forEach {
@@ -23,6 +17,23 @@ fun ConcatAdapter.contains(adapter: RecyclerView.Adapter<out RecyclerView.ViewHo
         }
     }
     return false
+}
+
+fun ConcatAdapter.addIfAbsent(adapter: RecyclerView.Adapter<out RecyclerView.ViewHolder>?) {
+    if (!contains(adapter)) {
+        add(adapter)
+    }
+}
+
+fun ConcatAdapter.addIfAbsent(index: Int, adapter: RecyclerView.Adapter<out RecyclerView.ViewHolder>?) {
+    if (!contains(adapter)) {
+        add(index, adapter)
+    }
+}
+
+fun ConcatAdapter.add(index: Int, adapter: RecyclerView.Adapter<out RecyclerView.ViewHolder>?) {
+    adapter ?: return
+    addAdapter(index, adapter)
 }
 
 fun ConcatAdapter.add(adapter: RecyclerView.Adapter<out RecyclerView.ViewHolder>?) {
