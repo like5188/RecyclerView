48ab206c0bc5a8b54c123f1961ca1fd836051cff
like5188 <260864866@qq.com>
2022-08-12 10:21:07 +0800

    showUiStatus 逻辑更改

--- ui/src/main/java/com/like/recyclerview/ui/adapter/BaseUiStatusController.kt
+++ ui/src/main/java/com/like/recyclerview/ui/adapter/BaseUiStatusController.kt
@@ -14,10 +14,10 @@ abstract class BaseUiStatusController(contentView: View) : UiStatusController(co
     var refresh: (suspend () -> Unit)? = null
 
     final override fun showUiStatus(tag: String) {// 加载中需要特殊处理
-        if (tag == getLoadingStatusTag() && !clickRefreshBtn) {
-            show?.invoke()// 如果不是点击的刷新按钮，那么就显示 show
+        if (tag == getLoadingStatusTag() && show != null && !clickRefreshBtn) {
+            show?.invoke()// 如果不是点击的刷新按钮，并且 show 不为 null，那么就显示 show
         } else {
-            super.showUiStatus(tag)// 如果点击的刷新按钮，那么就显示 loadingStatusUi
+            super.showUiStatus(tag)// 如果点击的刷新按钮，或者 show 为 null，那么就显示 loadingStatusUi
         }
     }
 
